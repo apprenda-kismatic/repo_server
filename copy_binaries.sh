a77630ca4765eb49d42995ecb7facacc95368b3c
BC Broussard <bc.broussard@flyingcranes.com>
2015-03-15 17:08:48 -0700

    copy binaries

---
+++ copy_binaries.sh
@@ -0,0 +1,5 @@
+# import release
+
+# copy other binaries from build location
+cp ../k8s_distro_builder/kubernetes/builds/*.rpm ./kismatic_builds/el/7/x86_64
+cp ../k8s_distro_builder/kubernetes/builds/*.deb ./kismatic_builds/debian/base
