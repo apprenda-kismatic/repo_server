2874170eaad956efaa74b1b987857f33926a021a
BC Broussard <bc.broussard@flyingcranes.com>
2015-03-15 22:44:58 -0700

    release process updates

--- copy_binaries.sh
+++ copy_binaries.sh
@@ -1,5 +1,7 @@
 # import release
 
 # copy other binaries from build location
-cp ../k8s_distro_builder/kubernetes/builds/*.rpm ./kismatic_builds/el/7/x86_64
-cp ../k8s_distro_builder/kubernetes/builds/*.deb ./kismatic_builds/debian/base
+cp -f ../k8s_distro_builder/kubernetes/builds/*.rpm ./kismatic_builds/el/7/x86_64
+cp -f ../k8s_distro_builder/kubernetes/builds/*.deb ./kismatic_builds/debian/base
+
+# copy install.sh files
--- release.sh
+++ release.sh
@@ -1,15 +1,16 @@
 # import release
 
+
 cd kismatic_builds
 #reprepro includedeb <osrelease> <debfile>
-reprepro -b /var/www/repos/apt/debian includedeb trusty debian/base/kubernetes*.deb
-reprepro -b /var/www/repos/apt/debian includedeb utopic debian/base/kubernetes*.deb
+reprepro -b /data/repos/apt/debian includedeb trusty debian/base/kubernetes*.deb
+reprepro -b /data/repos/apt/debian includedeb utopic debian/base/kubernetes*.deb
 
 # Yum
 cd el/7/x86_64
 rpm --resign *.rpm
 cd ../../../
-\cp -R el /var/www/repos/yum
+\cp -R el /data/repos/yum
 
 # createrepo for each one
-createrepo -d /var/www/repos/yum/el/7/x86_64
+createrepo -d /data/repos/yum/el/7/x86_64
