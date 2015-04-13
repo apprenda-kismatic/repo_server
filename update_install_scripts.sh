9ccd62a016703dbcba54b0c6bf45692d04a12701
BC Broussard <bc.broussard@flyingcranes.com>
2015-04-13 10:29:08 -0700

    update install scripts for get.kismatic.com

---
+++ update_install_scripts.sh
@@ -0,0 +1,5 @@
+# updates master and node install scripts for get.kismatic.com/kubernetes
+
+cd /data/kubernetes-distro-packages && git pull origin master
+#ln -s /data/kubernetes-distro-packages/kubernetes/install-master.sh /data/get.kismatic.com/public/kubernetes/master.sh
+#ln -s /data/kubernetes-distro-packages/kubernetes/install-node.sh /data/get.kismatic.com/public/kubernetes/node.sh
